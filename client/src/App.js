1377529c0245976d5e13de13d043a71fc0c7675d
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-03-24 11:20:18 +0300

    added tailwind dependancies

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 className="m-2 p-4 bg-blue-500 text-white ">Bloom Sayiorri</h1>
       </header>
     </div>
   );
