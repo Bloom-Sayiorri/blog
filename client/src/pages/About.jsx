c16d8a5ebe34089664c40a7f5de919223655a78b
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-03-30 19:40:54 +0300

    about section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className=''>
+      <div> 
+        <img src="" alt="About_Image" />
+      </div>
+      <h1 className='text-3xl font-extrabold text-center'>Blog App</h1>
+      <h3>How To Use:</h3>
+      <p>This is a simple blog app that allows us to read and interact with blogs on the go.</p>
+      <p>Feel free to read other peoples blog or create, update & delete your own blogs!</p>
+    </div>
   )
 }
 
