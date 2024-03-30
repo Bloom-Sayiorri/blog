3ad9d487c1ec069adb7af294a526535e07f36ee7
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-03-30 18:58:19 +0300

    created: about, profile and blog pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Blogs from './pages/Blogs';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/blogs" element={<Blogs/>} />
+        <Route exact path="/profile" element={<Profile/>} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   ) 
 }
