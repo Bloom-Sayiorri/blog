d15a53fd9040591dd0267885ac31d7ed42ad0491
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-03-30 19:55:27 +0300

    added about image

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import about from '../assets/about.jpg'
 
 const About = () => {
   return (
     <div className=''>
       <div> 
-        <img src="" alt="About_Image" />
+        <img src={about} alt="About_Image" />
       </div>
-      <h1 className='text-3xl font-extrabold text-center'>Blog App</h1>
-      <h3>How To Use:</h3>
-      <p>This is a simple blog app that allows us to read and interact with blogs on the go.</p>
-      <p>Feel free to read other peoples blog or create, update & delete your own blogs!</p>
+      <div>
+        <h1 className='text-3xl font-extrabold text-center'>Blog App</h1>
+        <h3>How To Use:</h3>
+        <p>This is a simple blog app that allows us to read and interact with blogs on the go.</p>
+        <p>Feel free to read other peoples blog or create, update & delete your own blogs!</p>
+      </div>
     </div>
   )
 }
