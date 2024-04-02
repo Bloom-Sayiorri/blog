69e15977656d38dcd7d4462d64934538edbc483d
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-04-02 19:18:29 +0300

    footer structure complete

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className='flex justify-center items-center'>
+      <div className='absolute bottom-0 mb-8'>
+        <p>&copy; 2024 Blog App. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
