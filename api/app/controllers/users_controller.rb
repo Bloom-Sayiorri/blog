2144059b4267c806d361575e9d2182b883abb604
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-04-02 17:57:07 +0300

    created a show action in rails api

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
     @user = User.all
     render json: @user
   end
+
+  def show
+    @user = find_user
+    render json: @user
+  end
+
+  private
+
+  def find_user
+    User.find(params[:id, :username, :email, :password])
+  end
+
 end
