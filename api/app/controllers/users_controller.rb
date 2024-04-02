a12e06d7bf2328bde2deac58636f285c501188c3
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-04-02 18:22:16 +0300

    show action refactored

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
-    render json: @user
+    @users = User.all
+    render json: @users
   end
 
   def show
     @user = find_user
-    render json: @user
+    if @user.present?
+      render json: @user
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
   end
 
   private
 
   def find_user
-    User.find(params[:id, :username, :email, :password])
+    User.find_by(id: params[:id])
   end
-
 end
