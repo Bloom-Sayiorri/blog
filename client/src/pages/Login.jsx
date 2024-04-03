a3b1c15dc8fd41225241600f15fb115746d9a7ab
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-04-03 16:23:07 +0300

    login page on client structure done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [formData, setFormData] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormData(formData);
+    }
+
+    const handleChange = (e) => {
+        e.target.value();
+    }
+
   return (
-    <div>Login</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <h2 className='text-4xl'>Login</h2>
+            <label htmlFor='username'>Username</label>
+            <input
+                type='text'
+                name='username'
+                id ='username'
+                placeholder='Username'
+                onChange={handleChange}
+                className='rounded-lg border-none py-2 px-4 max-w-80 w-48'
+            />
+            <label htmlFor='email'>Email</label>
+            <input
+                type='email'
+                name='email'
+                id = 'email'
+                placeholder='Email'
+                onChange={handleChange}
+                className='rounded-lg border-none py-2 px-4 max-w-80 w-48'
+            />
+            <label htmlFor='password'>Username</label>
+            <input
+                type='password'
+                name='password'
+                id = 'password'
+                placeholder='Password'
+                onChange={handleChange}
+                className='rounded-lg border-none py-2 px-4 max-w-80 w-48'
+            />
+        </form>
+    </div>
   )
 }
 
