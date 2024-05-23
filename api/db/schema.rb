31c83f0a3033328d35907d6933e66d01e3bb00da
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-05-23 22:22:05 +0300

    updated schema

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_23_191618) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_23_192017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_191618) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "user_id", null: false
+    t.bigint "blog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_reviews_on_blog_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -44,4 +54,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_191618) do
 
   add_foreign_key "blogs", "categories"
   add_foreign_key "blogs", "users"
+  add_foreign_key "reviews", "blogs"
+  add_foreign_key "reviews", "users"
 end
