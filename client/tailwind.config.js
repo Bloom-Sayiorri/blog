291887977ff894703c4d7d7a75b1f4eeff1f590f
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-06-16 20:13:24 +0300

    tailwind config configurations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'bg': '#1d1a39',
+      'soft': '#e8bcb9',
+      'dark-blue': '#384358',
+      'light': '#fbe4d8',
+      'dashboard': '#86abcf',
+    },
+    body: {
+      'margin': '0',
+      'padding': '0',
+      'box-sizing': 'border-box',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
