b1a397f30c9077619c7d59579fe8817c847756b8
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-06-16 21:41:13 +0300

    about page structure

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='flex justify-center items-center'>
-      <div className='absolute bottom-0 mb-8'>
-        <p>&copy; 2024 Blog App. All rights reserved.</p>
+    <footer className='flex justify-center items-center relative'>
+      <div className='fixed bottom-0 mb-8'>
+        <p>&copy; 2024 Blog App Inc. All rights reserved.</p>
       </div>
     </footer>
   )
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,15 +4,22 @@ import about from '../assets/about.jpg'
 const About = () => {
   return (
     <div className='h-full'>
-      <div> 
-        <img className='h-1/2' src={about} alt="About_Image" />
+      <h1 className='text-4xl font-semibold text-center'>Blog App</h1>
+
+      <div className='h-full flex justify-between items-center'>
+        <img className='h-40 w-40' src={about} alt="About_Image" />
+
+        <div className='h-3/5 bg-bg w-4'></div>
+
+        <div className=''>
+          <div className=''>
+            <p>How To Use:</p>
+            <p>This is a simple blog app that allows us to read and interact with blogs on the go.</p>
+            <p>Feel free to read other people's blog or create your own personalized blogs!</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1 className=''>Blog App</h1>
-        <h3>How To Use:</h3>
-        <p>This is a simple blog app that allows us to read and interact with blogs on the go.</p>
-        <p>Feel free to read other peoples blog or create, update & delete your own blogs!</p>
-      </div>
+
     </div>
   )
 }
