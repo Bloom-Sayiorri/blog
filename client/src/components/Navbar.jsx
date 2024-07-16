e33ee38c9a315b8664555ebcd9c3709b91c00702
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-07-16 18:04:58 +0300

    navbar section done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/logo.png';
+// import logo from '../assets/blog.jpg';
 import { FaMoon } from "react-icons/fa";
 import { LuSunDim } from "react-icons/lu";
 
 const Navbar = ({ isDark, handleDark }) => {
 
   return (
-    <header className='flex justify-between sticky'>
-      <div className='flex justify-center align-center text-center'>
-        <img className='h-20 w-20 text-center text-4xl' src={logo} alt="logo"/>
-        {/* <h1 className='font-black bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500/40 text-transparent bg-clip-text text-4xl'>Blog App</h1> */}
+    <header className='flex justify-between items-center py-2 px-4'>
+      <div className=''>
+        {/* <img src={logo} alt='Logo' height={40} width={40} className='bg-transparent' /> */}
+        <NavLink className=''>
+          <h1 className='font-semibold bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500/40 text-transparent bg-clip-text text-4xl'>Blog App</h1>
+        </NavLink>
       </div>
-      <nav>
+
+      <nav className='flex items-center gap-5'>
         <NavLink to='/home' className=''>Home</NavLink>
         <NavLink to='/blogs' className=''>Blogs</NavLink>
         <NavLink to='/profile' className=''>Profile</NavLink>
@@ -20,8 +23,9 @@ const Navbar = ({ isDark, handleDark }) => {
         <NavLink to='/signup' className=''>Signup</NavLink>
         <NavLink to='/login' className=''>Login</NavLink>
       </nav>
+
       <button type='button' onClick={handleDark}>
-        {isDark ? <LuSunDim className={`text-2xl ${isDark ? 'text-white' : 'text-black'}`}/> : <FaMoon className={`${isDark ? '' : 'text-zinc-700'}`}/>}
+        {isDark ? <LuSunDim className={`text-2xl ${isDark ? 'text-white' : ''}`}/> : <FaMoon className={`${isDark ? '' : 'text-black'}`}/>}
       </button>
     </header>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,28 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      bg: '#1d1a39',
-      soft: '#e8bcb9',
-      darkBlue: '#384358',
-      light: '#fbe4d8',
-      dashboard: '#86abcf',
-    },
-    body: {
-      margin: '0',
-      padding: '0',
-      boxSizing: 'border-box',
-    },
-    fontFamily: {
-      poppins: ['Poppins', 'sans-serif'],
-    },
     extend: {
+      colors: {
+        'bg': '#1d1a39',
+        'soft': '#e8bcb9',
+        'darkBlue': '#384358',
+        'light': '#fbe4d8',
+        'dashboard': '#86abcf',
+      },
     },
   },
   plugins: [],
