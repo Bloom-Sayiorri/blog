9e3880342eb5fe589a8f437cf1bffdef95b72e96
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-08-05 18:35:33 +0300

    validations

--- api/app/models/blog.rb
+++ api/app/models/blog.rb
@@ -1,14 +1,8 @@
 class Blog < ApplicationRecord
-  CATEGORIES = %w('Action', 'Tech', 'Sports', 'News')
+  # CATEGORIES = %w('Action', 'Tech', 'Sports', 'News')
 
   belongs_to :user
   belongs_to :category
 
-
-end
-=begin
-  validates :blog, presence: true, inclusion {
-    in: CATEGORIES,
-      message: "must be one of: #{CATEGORIES.join(', ')}"
-  }
+  # validates :genre, presence: true, inclusion { in: %w('Action', 'Tech', 'Sports', 'News'), message: "New blog must be one of: #{CATEGORIES.join(', ')}" }
 end
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :reviews
+  haas_many :blogs
+
+  vakidates :username, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true
 end
