712ed2fb924683fa30f133e4419fded421f03457
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-08-05 18:53:03 +0300

    model associations completed

--- api/app/models/blog.rb
+++ api/app/models/blog.rb
@@ -1,6 +1,7 @@
 class Blog < ApplicationRecord
   CATEGORIES = %w[Action Tech Sports News]
 
+  has_many :reviews
   belongs_to :user
 
   validates :category, presence: true, inclusion: { in: CATEGORIES, message: "New blog must be one of: #{CATEGORIES.join(', ')}" }
