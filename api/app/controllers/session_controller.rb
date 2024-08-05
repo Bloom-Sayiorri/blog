77dd9229db7c638805e29ac912045314605898ba
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-08-05 21:44:24 +0300

    session controller complete

--- api/app/controllers/session_controller.rb
+++ api/app/controllers/session_controller.rb
@@ -1,2 +1,22 @@
 class SessionController < ApplicationController
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      render json: @user, status: :created
+    else
+      render json: { errors: ["Invalid username or password!"] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session.destroy
+      head :no_content
+    else
+      render json: { errors: ["You are not logged in!"] }, status: :unauthorized
+    end
+  end
+  
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :blogs
   resources :users
 
-  post '/login', to: 'auth#create'
-  
+  post '/login', to: 'session#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
